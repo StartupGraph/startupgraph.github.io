1b6b638b7e1fb6054a47364347408a31596a7414
Billy Kwok <billykwok@resnet-119-113.ucdavis.edu>
2016-03-13 21:32:43 -0700

    remove redux-devtool

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,9 +1,7 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import ReduxThunk from "redux-thunk";
 import { initialState, combinedReducer } from "./Reducers";
 
-const Store = createStore(combinedReducer, initialState,
-  compose(applyMiddleware(ReduxThunk),
-    window.devToolsExtension ? window.devToolsExtension() : undefined));
+const Store = createStore(combinedReducer, initialState, applyMiddleware(ReduxThunk));
 
 export default Store;
